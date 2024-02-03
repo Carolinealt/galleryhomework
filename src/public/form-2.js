0aa8c04f70e058a69ef3943c97b1148d4c2cc0c9
Carolinealt <73542400+Carolinealt@users.noreply.github.com>
2024-02-03 18:12:45 +0200

    12313

--- src/public/form-2.js
+++ src/public/form-2.js
@@ -1,1 +1,30 @@
-refForm = document.querySelector(".feedback-form")
+const STORAGE_KEY = "feedback-form-state"
+
+const refForm = document.querySelector(".feedback-form")
+const refEmail = refForm.querySelector("input")
+const refTextArea = refForm.querySelector("textarea")
+console.log(refTextArea);
+popText()
+
+refForm.addEventListener("submit", onFormSubmint)
+refForm.addEventListener("input", textInput)
+
+function onFormSubmint(event){
+    event.preventDefault();
+    localStorage.removeItem(STORAGE_KEY);
+    event.currentTarget.reset();
+}
+
+function textInput(event){
+    const email = event.currentTarget.email.value
+    const text = event.currentTarget.elements.message.value
+    localStorage.setItem(STORAGE_KEY, JSON.stringify({email:email,text:text}))
+}
+function popText(){
+    const saveMessage = localStorage.getItem(STORAGE_KEY)
+    const curretnMessage = JSON.parse(saveMessage)
+    if(saveMessage){
+        refTextArea.value = curretnMessage.text
+        refEmail.value = curretnMessage.email
+    }
+}
